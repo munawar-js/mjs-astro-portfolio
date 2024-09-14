58f0d5e6f17e34e44713a16496e0f5b1893b1149
“Munawar <6573105+munawar-js@users.noreply.github.com>
2024-09-14 16:42:46 +0530

    Revert "Add: tina CMS"
    This reverts commit c9e192b8db48d3b01dfc7d6edb5747016ae4008b.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URL } from "./src/data/config";
 import vercel from '@astrojs/vercel/serverless';
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
